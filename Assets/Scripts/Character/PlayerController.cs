19253ab68fc65e7010272642703887af63bad963
Huynh Anh <66988477+vampireplus123@users.noreply.github.com>
2025-01-18 16:39:29 +0700

    update-and-import new Player Controller

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -1,18 +1,29 @@
+using System.Xml;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10f; // Tốc độ di chuyển
+    public float PlayerMovementSpeed = 10f; // Tốc độ di chuyển
+    public float PlayerRotateSpeed = 10f;
     Rigidbody rb;
 
     // Khởi tạo
     void Start()
     {
+        if(gameObject.GetComponent<Rigidbody>() == null)
+        {
+            gameObject.AddComponent<Rigidbody>();
+        }
         rb = gameObject.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
     }
 
     // Gọi mỗi frame
     void Update()
+    {
+
+    }
+    void FixedUpdate()
     {
         Move();
     }
@@ -27,6 +38,11 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(horizontal, 0f, vertical).normalized;
 
         // Di chuyển Player
-        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+        if(movement.magnitude > 0.1f)
+        {
+            rb.MovePosition(rb.position + movement * PlayerMovementSpeed * Time.deltaTime);
+            Quaternion PlayerRotation = Quaternion.LookRotation(movement);
+            rb.rotation = Quaternion.Slerp(rb.rotation,PlayerRotation,PlayerRotateSpeed*Time.fixedDeltaTime);
+        }
     }
 }
